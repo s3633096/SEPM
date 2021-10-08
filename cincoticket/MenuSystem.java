a1dd3c5d1026a35fc67db9d96ea44b4d1f8134b4
David Lillie <david.lillie@live.com>
2021-10-08 16:07:07 +1100

    Add user class
    Added user class, seed hardcoded data on startup, added basic login method.

--- cincoticket/MenuSystem.java
+++ cincoticket/MenuSystem.java
@@ -1,10 +1,33 @@
 public final class MenuSystem {
 	
+	private User[] users = new User[5];
+	
 	public MenuSystem() {
-
+		SeedUsers();
 	}
 	
 	public void Start() {
 		System.out.println("Please select from the following options:");
+	}
+
+	
+	private void SeedUsers() {
+		users[0] = new User("harry_styles@cinco.com", "Harry Styles", "0400123456", "123abc");
+		users[1] = new User("niall_horan@cinco.com", "Niall Horan", "0400125555", "456def");
+		users[2] = new User("liam_payne@cinco.com", "Liam Payne", "0404123499", "789ghi");
+		users[3] = new User("louis_tomlinson@cinco.com", "Louis Tomlinson", "0407126456", "abc321");
+		users[4] = new User("zayn_malik@cinco.com", "Zayn Malik", "0411223456", "654fed");
+	}
+	
+	private User Login(String email, String password) {
+		for (int i = 0; i < users.length; i++) {
+			if (email == users[i].getEmail()) {
+				if (password == users[i].getPassword()) {
+					return users[i];
+				}
+				else break;
+			}
+		}
+		return null;
 	}
 }
---
+++ cincoticket/User.java
@@ -0,0 +1,30 @@
+
+public class User {
+	private String email;
+	private String name;
+	private String phone;
+	private String password;
+	
+	public User(String email, String name, String phone, String password) {
+		this.email = email;
+		this.name = name;
+		this.phone = phone;
+		this.password = password;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+}
