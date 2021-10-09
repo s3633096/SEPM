24290775053bf7d825c6b617a1eba46be73e091a
Connievon2003 <s3777909@student.rmit.edu.au>
2021-10-09 15:54:43 +1100

    change passwords to requirements (just in case)

--- cincoticket/MenuSystem.java
+++ cincoticket/MenuSystem.java
@@ -35,11 +35,11 @@ public final class MenuSystem {
 
 	
 	private void SeedUsers() {
-		users[0] = new User("harry_styles@cinco.com", "Harry Styles", "0400123456", "123abc");
-		users[1] = new User("niall_horan@cinco.com", "Niall Horan", "0400125555", "456def");
-		users[2] = new User("liam_payne@cinco.com", "Liam Payne", "0404123499", "789ghi");
-		users[3] = new User("louis_tomlinson@cinco.com", "Louis Tomlinson", "0407126456", "abc321");
-		users[4] = new User("zayn_malik@cinco.com", "Zayn Malik", "0411223456", "654fed");
+		users[0] = new User("harry_styles@cinco.com", "Harry Styles", "0400123456", "123abcABC123abcABC12");
+		users[1] = new User("niall_horan@cinco.com", "Niall Horan", "0400125555", "456defDEF456defDEF45");
+		users[2] = new User("liam_payne@cinco.com", "Liam Payne", "0404123499", "789ghiGHI789ghiGHI78");
+		users[3] = new User("louis_tomlinson@cinco.com", "Louis Tomlinson", "0407126456", "cba321CBA321cbaCBA32");
+		users[4] = new User("zayn_malik@cinco.com", "Zayn Malik", "0411223456", "654fedFED654fedFED65");
 	}
 	
 	private void Login() {
